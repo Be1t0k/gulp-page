e2053583f85ee84cae4fe45ad563322fe9df0201
Be1t0k <pasha.moshckov@yandex.ru>
2022-11-04 15:20:53 +0300

    last video

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,3 +1,14 @@
-function name() {
-    $('.mask-phone').mask('+7 (999) 999-99-99');
-}
+$.mask.definitions['h'] = "[0|1|3|4|5|6|7|9]"
+$(".mask-phone").mask("+7 (h99) 999-99-99");
+
+
+const Form = document.getElementById("formid")
+Form.addEventListener('submit', handleFormSubmit)
+function handleFormSubmit(event) {
+    // Просим форму не отправлять данные самостоятельно
+    event.preventDefault()
+    alert('Отправка!')
+  }
+
+
+
